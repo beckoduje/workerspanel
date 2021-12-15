946bbe087fd8ab37cef3d2ad33030817e6800fc9
beckoduje <dujebecko@gmail.com>
2021-12-15 17:41:25 +0200

    day4
    Deinstalirao Redux i Toolkit. Odradit ću prvo projekt s context api a potom napraviti verziju koda sa reduxom

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,12 +1,7 @@
 import React from "react";
 import LogInForm from "../components/log-in/LogInForm";
 
-// testni primjer
-// import { useSelector } from "react-redux";
-
 export default function LogIn() {
-  // const isLoggedIn = useSelector((state) => state.isLogged.isLogged);
-  // console.log(isLoggedIn);
   return (
     <section className="log-in-section">
       <LogInForm />
--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import isLoggedReducer from "./isLogged";
-
-const store = configureStore({
-  reducer: { isLogged: isLoggedReducer },
-});
-
-export default store;
